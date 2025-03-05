0ceebed704d87faed4c4e4491165982ab196ad2e
shital satpute <satputeshitald@gmail.com>
2025-03-05 22:37:14 +0530

    Create email connection
    Create email connection

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -3,16 +3,27 @@ import './App.css'
 import { LoadingScreen } from './Components/LoadingScreen'
 import { useState } from 'react';
 import { Navbar } from './Components/Sections/Navbar';
+import { MobileMenu } from './Components/Sections/MobileMenu';
+import { Home } from './Components/Sections/Home';
+import { About } from './Components/Sections/About';
+import { Projects } from './Components/Sections/Project';
+import { Contact } from './Components/Sections/Contact';
 
 function App() {
 
   const [isloaded, setIsLoaded]=useState(false);
-  
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
     {!isloaded && <LoadingScreen onComplete={()=> setIsLoaded(true)}/>}
      <div className={`min-h-screen transition-opacity duration-700 ${isloaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
-        <Navbar/>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <Home/>
+        <About/>
+        <Projects/>
+        <Contact/>
      </div>
     </>
   )
